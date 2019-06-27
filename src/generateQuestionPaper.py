68781c4040859853fa3ce4a5a9f9a7ba8e25a010
Hardik Mahipal Surana <hardik@criodo.com>
2019-06-27 23:05:38 +0530

    questions shown 1 by 1 and responses collected

--- src/generateQuestionPaper.py
+++ src/generateQuestionPaper.py
@@ -30,21 +30,50 @@ import argparse
 from cutImage import Cut
 import json
 
-def generate_questions(sets, questionNum, polyNum, optionNum):
-    questions = dict()
+# def generate_questions(sets, questionNum, polyNum, optionNum):
+#     questions = dict()
 
-    for s in range(1, sets + 1):
-        questions["set_" + str(s)] = dict()
-        if questionNum is not None:
-            for i in range(1, questionNum + 1):
-                c = Cut(polyNum, i, s, optionNum)
-                questions["set_" + str(s)]['q' + str(i)] = dict()
-                questions["set_" + str(s)]['q' + str(i)]['question_type'] = 'cut'
-                c.genQuestionAnswerPair()
-                c.genDistractors()
-                questions["set_" + str(s)]['q' + str(i)]['question'] = c.getQuestion()
-                questions["set_" + str(s)]['q' + str(i)]['answer'] = c.getAnswer()
-                questions["set_" + str(s)]['q' + str(i)]['distractors'] = c.getDistractors()
+#     for s in range(1, sets + 1):
+#         questions["set_" + str(s)] = dict()
+#         if questionNum is not None:
+#             for i in range(1, questionNum + 1):
+#                 c = Cut(polyNum, i, s, optionNum)
+#                 questions["set_" + str(s)]['q' + str(i)] = dict()
+#                 questions["set_" + str(s)]['q' + str(i)]['question_type'] = 'cut'
+#                 c.genQuestionAnswerPair()
+#                 c.genDistractors()
+#                 questions["set_" + str(s)]['q' + str(i)]['question'] = c.getQuestion()
+#                 questions["set_" + str(s)]['q' + str(i)]['answer'] = c.getAnswer()
+#                 questions["set_" + str(s)]['q' + str(i)]['distractors'] = c.getDistractors()
+#     # print json.dumps(questions, sort_keys=True, indent=4)
+#     return questions
+
+
+'''
+# format to store quiz data in dictionary
+{
+    '<Question_Number>' : {
+        'question_type': 'cut' / 'fold' / 'dice' ...,
+        'question_text': 'identify the missing piece of the image'
+        'question': <Path_To_Question_Image>,
+        'answer': <Path_To_Solution_Image>,
+        'distractors': ['<Path_To-Dist_1>','<Path_To-Dist_2>','<Path_To-Dist_3>']
+    }
+}
+'''
+def generate_questions(questionNum, polyNum, optionNum):
+    questions = dict()
+    if questionNum is not None:
+        for i in range(1, questionNum + 1):
+            c = Cut(polyNum, i, 1, optionNum)
+            questions[str(i)] = dict()
+            questions[str(i)]['question_type'] = 'cut'
+            questions[str(i)]['question_text'] = 'identify the missing piece of the image'
+            c.genQuestionAnswerPair()
+            c.genDistractors()
+            questions[str(i)]['question'] = c.getQuestion()
+            questions[str(i)]['answer'] = c.getAnswer()
+            questions[str(i)]['distractors'] = c.getDistractors()
     # print json.dumps(questions, sort_keys=True, indent=4)
     return questions
 
@@ -59,7 +88,8 @@ if __name__ == "__main__":
     # parser.add_argument('--series', type=int, help='enter number of \'series\' questions to be generated')
     parser.add_argument('--polyNum', required=True, type=int, help='enter number of polygons embedded in outer polygon')
     parser.add_argument('--optionNum', type=int, help='enter number of options to be generated along with the answer')
-    parser.add_argument('--sets', type=int, help='enter number of question paper sets to be generated')
+    # parser.add_argument('--sets', type=int, help='enter number of question paper sets to be generated')
 
     args= parser.parse_args()
-    questions = generate_questions(args.sets, args.cut, args.polyNum, args.optionNum)
+    # questions = generate_questions(args.sets, args.cut, args.polyNum, args.optionNum)
+    questions = generate_questions(args.cut, args.polyNum, args.optionNum)

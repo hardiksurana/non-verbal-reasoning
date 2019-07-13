9ea795e5c2d799edd90b5975d0b643fd6c5299a5
Hardik Mahipal Surana <hardik@criodo.com>
2019-07-13 15:03:41 +0530

    updated db to point to AWS

--- src/webapp/mysql_utils.py
+++ src/webapp/mysql_utils.py
@@ -4,11 +4,19 @@ from mysql.connector import errorcode, Error
 class MySQL:
     def __init__(self):
         self.conn = mysql.connector.connect(
-                host="localhost",
-                user="turtle",
+                host="nvrquiz-db-instance.chb3ppjrdtjp.ap-south-1.rds.amazonaws.com",
+                port=3306,
                 database="non_verbal_reasoning",
-                passwd="turtle"
+                user="turtle",
+                password="NVRqu!z123"
         )
+
+        # self.conn = mysql.connector.connect(
+        #         host="localhost",
+        #         user="turtle",
+        #         database="non_verbal_reasoning",
+        #         passwd="turtle"
+        # )
 
         self.curr = self.conn.cursor(buffered=True, dictionary=True)
     

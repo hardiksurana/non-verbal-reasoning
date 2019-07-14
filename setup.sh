f3612ea3b56d6d9165b1ca087cb3b9f55048e5f0
Hardik Mahipal Surana <hardik@criodo.com>
2019-07-14 14:59:55 +0530

    updated setup.sh to remove apt-get commands to prevent build failure

--- setup.sh
+++ setup.sh
@@ -1,1 +1,2 @@
-echo "starting custom startup commands" && apt-get install libpng-dev libfreetype6 pkg-config libfreetype6-dev libpng-dev python-matplotlib
+echo "starting custom startup commands from setup.sh set by Hardik"
+# apt-get install libpng-dev libfreetype6 pkg-config libfreetype6-dev libpng-dev python-matplotlib

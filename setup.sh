597d3b86d02c8c069ad95eec04df87e14f9a5fd4
Hardik Mahipal Surana <hardik@criodo.com>
2019-07-14 17:21:31 +0530

    added new file to test Azure file updation

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
-echo "starting custom startup commands from setup.sh set by Hardik"
+echo "Hardik says hi!"
 # apt-get install libpng-dev libfreetype6 pkg-config libfreetype6-dev libpng-dev python-matplotlib
---
+++ test.py
@@ -0,0 +1,2 @@
+# this is a simple test file added just to check if MS Azure App Service picks up this file when pushed
+# it is not used anywhere in the code and can be safely deleted

2796ea8f11acee880d7974974d5f62a2b9e8d97e
Hardik Mahipal Surana <hardik@criodo.com>
2019-07-22 23:03:36 +0530

    removed localhost and added AWS db instance

--- src/webapp/mysql_utils.py
+++ src/webapp/mysql_utils.py
@@ -11,36 +11,23 @@ self.conn = mysql.connector.connect(
 '''
 class MySQL:
     def __init__(self):
-        '''self.conn = mysql.connector.connect(
+        self.conn = mysql.connector.connect(
                 host="nvrquiz-db-instance.chb3ppjrdtjp.ap-south-1.rds.amazonaws.com",
                 port=3306,
                 database="non_verbal_reasoning",
                 user="turtle",
                 password="NVRqu!z123"
-        )'''
-        self.conn = mysql.connector.connect(
-                host="localhost",
-                user="turtle",
-                database="non_verbal_reasoning",
-                passwd="turtle"
         )
 
         self.curr = self.conn.cursor(buffered=True, dictionary=True)
     
     def create_connection(self):
-        '''self.conn = mysql.connector.connect(
+        self.conn = mysql.connector.connect(
                 host="nvrquiz-db-instance.chb3ppjrdtjp.ap-south-1.rds.amazonaws.com",
                 port=3306,
                 database="non_verbal_reasoning",
                 user="turtle",
                 password="NVRqu!z123"
-        )'''
-
-        self.conn = mysql.connector.connect(
-                host="localhost",
-                user="turtle",
-                database="non_verbal_reasoning",
-                passwd="turtle"
         )
 
     def close_connection(self):
